16b8cb04d12ba6fa4b0a6ca73ec372d96c13e08c
SRCrimson <92110773+SRCrimson@users.noreply.github.com>
2021-11-02 21:45:31 -0500

    Create Marca.java

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,15 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	
+	public Marca(String name) {
+		nombre = name;
+	}
+	public String getNombre() {
+		return name;
+	}
+	public void setNombre(String name) {
+		nombre = name;
+	}
+}
